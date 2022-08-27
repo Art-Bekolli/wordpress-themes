fef7d928029ae1b5401ace917f1e478e8b35b90f
Art-Bekolli <104490411+Art-Bekolli@users.noreply.github.com>
2022-08-27 18:38:06 +0200

    Add files via upload

--- noperdenoparty-theme/js/main.js
+++ noperdenoparty-theme/js/main.js
@@ -2,38 +2,41 @@ var viewpr = document.getElementById('view-products');
 var postdesc = document.getElementsByClassName('recent-posts')[0];
 var numer = 4;
 
-
+//get info me JSON
 function ShowMore(){
-var myRequest = new XMLHttpRequest();
-myRequest.open('GET', 'http://localhost/wordpress/wp-json/wp/v2/posts?per_page='+ numer +'&_embed');
-myRequest.onload = function(){
-	if (myRequest.status >= 200 && myRequest.status < 400){
-		var data = JSON.parse(myRequest.responseText);
-		makeHTML(data);
-	} else{
-		console.log("nifar errori");
-	}
-};
-
-myRequest.onerror = function(){
-	console.log("connection error");
-};
-
-myRequest.send();
-
+fetch('http://localhost/wordpress/wp-json/wp/v2/posts?per_page='+ numer +'&_embed')
+.then(data => {
+return data.json();
+})
+.then(post => {
+	//call function me output data in html
+makeHTML(post);
+});
 }
 
-function makeHTML(postsData){
+
+
+
+
+
+//displaying data nfrontend
+function makeHTML(post){
 var HTMLString = '';
-for (i=0; i < postsData.length; i++){
-	HTMLString += '<div class="post"><div id="thumbnail"> <img width="320" height="400" style="object-fit: cover;" src="' + postsData[i]._embedded['wp:featuredmedia']['0'].source_url + '"></div>';
-	HTMLString += '<div id="title"><a href="' + postsData[i].link +'">' + postsData[i].title.rendered + '</a></div></div>';
+for (i=0; i < post.length; i++){
+	HTMLString += '<div class="post"><div id="thumbnail"> <img width="320" height="400" style="object-fit: cover;" src="' + post[i]._embedded['wp:featuredmedia']['0'].source_url + '"></div>';
+	HTMLString += '<div id="title"><a href="' + post[i].link +'">' + post[i].title.rendered + '</a></div></div>';
 	
 }
 postdesc.innerHTML = HTMLString;
 }
 
+
+
+
+
+
 viewpr.addEventListener('click', function(){
 	numer += 4;
 	ShowMore();
 });
+
